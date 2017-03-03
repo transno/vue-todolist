c5a9ccae11ec61bbf56a3585c15efc52396cebf2
JohnnyR <ymwdleifeng@163.com>
2017-03-03 09:36:21 +0800

    列表删除

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,46 +2,55 @@
   <div class="hello">
     <input type="text" v-model="text" placeholder="输入回车添加Todolist" @keyup.enter="addList">
     <ul>
-      <li v-for="items in list">{{items.msg}}</li>
+      <li v-for="items in list"><span>{{items.msg}}</span><i @click="removeList(items)">×</i></li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      text: '',
-      list: []
-    }
-  },
-  methods: {
-    addList () {
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        text: '',
+        list: []
+      }
+    },
+    methods: {
+      addList () {
         this.list.push({msg: this.text});
         this.text = '';
+      },
+      removeList (i) {
+        this.list.splice(i, 1);
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: block;
-  margin: 10px;
-}
-
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: block;
+    margin: 10px auto;
+    position: relative;
+    width: 200px;
+  }
+  i {
+    position: absolute;
+    display: block;
+    top: 0;
+    right: 0;
+    cursor: pointer;
+  }
+  a {
+    color: #42B983;
+  }
 </style>
